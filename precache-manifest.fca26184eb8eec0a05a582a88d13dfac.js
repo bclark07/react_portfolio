f89dd892ccb651dd6807d9922e4d64e7cad86b5d
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-22 00:39:30 -0400

    Updates

---
+++ precache-manifest.fca26184eb8eec0a05a582a88d13dfac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17c06c8f5843755ced0c5cd4a9fb7b1",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "f45241be388c1c46ba97",
+    "url": "/react_portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9ac667ceb082c167dab2",
+    "url": "/react_portfolio/static/js/2.78e1a0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio/static/js/2.78e1a0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45241be388c1c46ba97",
+    "url": "/react_portfolio/static/js/main.2b81e7e0.chunk.js"
+  },
+  {
+    "revision": "d0d7ed5cb713b0b499cb",
+    "url": "/react_portfolio/static/js/runtime-main.5aac7c27.js"
+  }
+]);
