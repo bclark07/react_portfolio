368e43ec2d189cdae4da68dcad5e0b7d80d1106c
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-23 14:41:15 -0400

    Updates

---
+++ precache-manifest.72a8ef8039b802041d76dba5b054d470.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cde6be7b46c6328bbe327ac464c02a",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "7f0556c7341ebd30eee7",
+    "url": "/react_portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c3317338b66143eb0e95",
+    "url": "/react_portfolio/static/js/2.20e6e7b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio/static/js/2.20e6e7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0556c7341ebd30eee7",
+    "url": "/react_portfolio/static/js/main.bbf0df93.chunk.js"
+  },
+  {
+    "revision": "d0d7ed5cb713b0b499cb",
+    "url": "/react_portfolio/static/js/runtime-main.5aac7c27.js"
+  },
+  {
+    "revision": "886994be90a5bb6eb29531afa7776d71",
+    "url": "/react_portfolio/static/media/Burger.886994be.jpg"
+  },
+  {
+    "revision": "21922bfc40bebd020b9e93650505125e",
+    "url": "/react_portfolio/static/media/ClarkPhoto.21922bfc.jpg"
+  },
+  {
+    "revision": "d5ea1436558f3b9a829b79892e9740db",
+    "url": "/react_portfolio/static/media/Employee.d5ea1436.png"
+  },
+  {
+    "revision": "1dbe84a91bf451a6e63de6626d3cfd4b",
+    "url": "/react_portfolio/static/media/Restaurant.1dbe84a9.jpg"
+  }
+]);
