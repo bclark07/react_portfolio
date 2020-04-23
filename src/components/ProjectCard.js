3f25eecca83fd4faa20ae52c88d71e5406b66470
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-23 14:40:54 -0400

    project cards formatted

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,29 +4,32 @@ import React from "react";
 // function ProjectCard(props) {
 function ProjectCard(props) {
   return (
-    <div className="card">
-      <div className="card-body">
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+    <div className="card border-secondary">
+      <div className="text-center">
+        <h3>{props.project_name}</h3>
       </div>
-      <div className="img-container">
-        <img alt={props.project_name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Project Title:</strong> {props.project_name}
-          </li>
-          <li>
-            <strong>Skills:</strong> {props.languages}
-          </li>
-          <li>
-            <strong>View:</strong> <a>{props.url}</a>
-            {/* //figure out how to pass a url  */}
-          </li>
-        </ul>
+      <div className="card border-white img-container">
+        <img
+          alt={props.project_name}
+          src={props.image}
+          className="card-img-top border border-secondary"
+        />
+        <div className="card-body p-0">
+          <ul className="text-left list-group">
+            <li className="list-group-item p-0 border-white">
+              <strong>Project Title:</strong> {props.project_name}
+            </li>
+            <li className="list-group-item p-0 border-white">
+              <strong>Skills:</strong> {props.languages}
+            </li>
+            <li className="list-group-item p-0 border-white">
+              <strong>View: </strong>
+              <a href={props.url} target="_blank" className="card-link">
+                Deployed Link
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,39 +1,37 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import projectlist from "../projectlist.json";
-import picture1 from "../images/ClarkPhoto.jpg"; // How get image to load
-import picture2 from "../images/Burger.jpg"; // How get image to load
-
-const photos = [{ picture1 }, { picture2 }];
 
 const Projects = () => {
   return (
-    <div>
+    <div className="container text-center">
       <p>Click on a Project to learn more</p>
-      <ProjectCard
-        project_name={projectlist[0].project_name}
-        image={require("../images/github.png")}
-        languages={projectlist[0].languages}
-        url={projectlist[0].url}
-      />
-      <ProjectCard
-        project_name={projectlist[1].project_name}
-        image={require("../images/Burger.jpg")}
-        languages={projectlist[1].languages}
-        url={projectlist[1].url}
-      />
-      <ProjectCard
-        project_name={projectlist[2].project_name}
-        image={require("../images/Employee.png")}
-        languages={projectlist[2].languages}
-        url={projectlist[2].url}
-      />
-      <ProjectCard
-        project_name={projectlist[2].project_name}
-        image={require("../images/Restaurant.jpg")}
-        languages={projectlist[2].languages}
-        url={projectlist[2].url}
-      />
+      <div className="row card-deck">
+        <ProjectCard
+          project_name={projectlist[0].project_name}
+          image={require("../images/github.png")}
+          languages={projectlist[0].languages}
+          url={projectlist[0].url}
+        />
+        <ProjectCard
+          project_name={projectlist[1].project_name}
+          image={require("../images/Burger.jpg")}
+          languages={projectlist[1].languages}
+          url={projectlist[1].url}
+        />
+        <ProjectCard
+          project_name={projectlist[2].project_name}
+          image={require("../images/Employee.png")}
+          languages={projectlist[2].languages}
+          url={projectlist[2].url}
+        />
+        <ProjectCard
+          project_name={projectlist[3].project_name}
+          image={require("../images/Restaurant.jpg")}
+          languages={projectlist[3].languages}
+          url={projectlist[3].url}
+        />
+      </div>
     </div>
   );
 };
