df51a6e57837999df5d4cd7dba3210e5ffa83dca
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-23 14:09:46 -0400

    Project cards loading with data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import projectlist from "../projectlist.json";
+import picture1 from "../images/ClarkPhoto.jpg"; // How get image to load
+import picture2 from "../images/Burger.jpg"; // How get image to load
+
+const photos = [{ picture1 }, { picture2 }];
 
 const Projects = () => {
   return (
@@ -8,19 +12,25 @@ const Projects = () => {
       <p>Click on a Project to learn more</p>
       <ProjectCard
         project_name={projectlist[0].project_name}
-        image={projectlist[0].image}
+        image={require("../images/github.png")}
         languages={projectlist[0].languages}
         url={projectlist[0].url}
       />
       <ProjectCard
         project_name={projectlist[1].project_name}
-        image={projectlist[1].image}
+        image={require("../images/Burger.jpg")}
         languages={projectlist[1].languages}
         url={projectlist[1].url}
       />
       <ProjectCard
         project_name={projectlist[2].project_name}
-        image={projectlist[2].image}
+        image={require("../images/Employee.png")}
+        languages={projectlist[2].languages}
+        url={projectlist[2].url}
+      />
+      <ProjectCard
+        project_name={projectlist[2].project_name}
+        image={require("../images/Restaurant.jpg")}
         languages={projectlist[2].languages}
         url={projectlist[2].url}
       />
---
+++ src/imgObj.js
@@ -0,0 +1,7 @@
+const imgObj = {
+    note: require('./images/github.png')
+    burger: require('./images/Burger.jpg'),
+    employee: require('./images/Employee.png'),
+    restaurant: require('./images/Restaurant.jpg')
+  }
+  module.exports = imgObj
